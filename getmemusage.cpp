a13bfc17ae2edb5c109f40ce252ecad5764fb317
Tamas Kenez <tamas.kenez@kishonti.net>
2014-05-26 14:26:09 +0200

    add macosx getmemusage()

--- getmemusage.cpp
+++ getmemusage.cpp
@@ -17,10 +17,17 @@ size_t GetMemUsage()
 
 #else
 
+#include <mach/mach.h>
+
 size_t GetMemUsage()
 {
-	// unimplemented. used only for profiling
-	return 0;
+    struct mach_task_basic_info ti;
+    memset(&ti, 0, sizeof(ti));
+    mach_msg_type_number_t count = MACH_TASK_BASIC_INFO_COUNT;
+    kern_return_t kr = task_info(mach_task_self(), MACH_TASK_BASIC_INFO,
+                                 (task_info_t) &ti, &count);
+	
+    return kr == KERN_SUCCESS ? ti.virtual_size : 0;
 }
 
 #endif
