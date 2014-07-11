c293bbb65929fd83979cff7a826cbfb931017a67
tamas.kenez <tamas.kenez@kishonti.net>
2014-07-11 18:28:16 +0200

    tmp commit

--- config.h
+++ config.h
@@ -9,18 +9,18 @@
 #define MAPPED_TYPE_SIZE sizeof(KEY_TYPE)
 
 //mem test will test hash maps with data size in this range
-const size_t kMemTest_rawDataMBytes_min = 100;
-const size_t kMemTest_rawDataMBytes_max = 200;
+const size_t kMemTest_rawDataMBytes_min = 1;
+const size_t kMemTest_rawDataMBytes_max = 2;
 
 //multiple-element-count speed test will test in this range
 //single-element-count speed test will test at max
-const size_t kSpeedTest_rawDataMBytes_min = 20;
-const size_t kSpeedTest_rawDataMBytes_max = 200;
+const size_t kSpeedTest_rawDataMBytes_min = 1;
+const size_t kSpeedTest_rawDataMBytes_max = 2;
 
 //One test may run for different item counts
 //(hash map sizes). The item count will be the x-axis.
 //This value determines the resolition along x-axis.
-const size_t kNumRunsPerTest = 12;
+const size_t kNumRunsPerTest = 24;
 
 // ---- //
 
